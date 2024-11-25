a8a48ad6bab92e67ec42abbf87bf1e4bd23f647c
Nikola Vlaskalin <nikola.vlaskalin00@gmail.com>
2024-11-25 12:43:44 +0100

    Expanded bash script to use docker compose instead of docker

--- modules/ec2/user_data/script.sh
+++ modules/ec2/user_data/script.sh
@@ -15,24 +15,37 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-echo ${DOCKER_USERNAME}
-echo ${DOCKER_PASSWORD}
+sudo groupadd docker
+sudo usermod -aG docker $USER 
 
-sudo docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+sudo su - $USER
 
-sudo docker pull grtalca/python-app
+sudo systemctl restart docker
+
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+
+docker pull grtalca/python-app
 
 IP_ADDR=$(curl https://ifconfig.me/)
 
-sudo docker container run -d \
-    -e DATABASE_HOSTNAME=${DATABASE_HOSTNAME} \
-    -e DATABASE_NAME=${DATABASE_NAME} \
-    -e DATABASE_PORT=${DATABASE_PORT} \
-    -e DATABASE_USERNAME=${DATABASE_USERNAME} \
-    -e DATABASE_PASSWORD=${DATABASE_PASSWORD} \
-    -e SECRET_KEY=${SECRET_KEY} \
-    -e ALGORITHM=${ALGORITHM} \
-    -p 8080:8080 \
-    --name python-web \
-    grtalca/python-app \
-    bash -c "alembic upgrade head && gunicorn app.main:app -w 3 -b 0.0.0.0:8080 -k uvicorn.workers.UvicornWorker"
+mkdir /home/$USER/docker
+cd /home/$USER/docker
+
+cat <<EOF > docker-compose.yaml
+version: "3.9"
+services:
+  python-application:
+    image: grtalca/python_app
+    container_name: python-application
+    environment:
+      DATABASE_HOSTNAME: ${DATABASE_HOSTNAME}
+      DATABASE_NAME: ${DATABASE_NAME}
+      DATABASE_PORT: ${DATABASE_PORT}
+      DATABASE_USERNAME: ${DATABASE_USERNAME}
+      DATABASE_PASSWORD: ${DATABASE_PASSWORD}
+      SECRET_KEY: ${DATABASE_SECRET_KEY}
+      ALGORITHM: ${DATABASE_ALGORITHM}
+    command: bash -c "alembic upgrade head && gunicorn app.main:app -w 3 -b 0.0.0.0:8080 -k uvicorn.workers.UvicornWorker"
+EOF
+
+docker compose up -d 
