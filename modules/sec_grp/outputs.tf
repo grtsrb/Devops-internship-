b1066f7032f34c8c2152912a718dcb354b087aed
Nikola Vlaskalin <nikola.vlaskalin00@gmail.com>
2024-11-25 08:59:17 +0100

    Clean branch

--- modules/sec_grp/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "pub_sg_id" {
-  value = aws_security_group.public_security_group.id
-}
-
-output "priv_sg_id" {
-  value = aws_security_group.private_security_group.id
-}
--- modules/subnets/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "pub_subnet_id" {
-  value = aws_subnet.public_subnet[*].id
-}
-
-output "priv_subnet_id" {
-  value = aws_subnet.private_subnet[*].id
-}
